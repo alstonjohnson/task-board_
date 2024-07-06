08f07aa96b01424b9a9d5baf885ade899976ed45
alstonjohnson <johnson.alston@icloud.com>
2024-07-06 15:19:12 -0500

    add card description p tag

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -14,6 +14,7 @@ function createTaskCard(task) {
         .attr('data-task-id', task.id);
     const cardHeader = $('<div>').addClass('card-header h3').text(task.name);
     const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(task.tpye);
 
 }
 
