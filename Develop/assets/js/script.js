23769275f6877f495b54bfd0c56908dc4be3ca55
alstonjohnson <johnson.alston@icloud.com>
2024-07-07 15:20:12 -0500

    updated random ID generator code

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -5,6 +5,7 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
     id: crypto.randomUUID();
+    return;
 }
 
 // Todo: create a function to create a task card
