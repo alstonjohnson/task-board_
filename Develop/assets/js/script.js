e75f6ab6a0785f45e9fd3e61931af6a409cde198
alstonjohnson <johnson.alston@icloud.com>
2024-07-09 10:47:44 -0500

    find by ID and update status

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -114,6 +114,12 @@ function handleDrop(event, ui) {
 
     const newStatus = event.target.id
 
+    for (let task of tasks) {
+        if (task.id === taskID) {
+            task.status = newStatus;
+        }
+    }
+
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
