997b32a22a0a3f615d7f227873caaa45b6b941ba
alstonjohnson <johnson.alston@icloud.com>
2024-07-09 10:51:06 -0500

    compeleted inital work on hand drop

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -120,6 +120,9 @@ function handleDrop(event, ui) {
         }
     }
 
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    printTaskData();
+
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
